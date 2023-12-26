d1a99168c8207367b624c80e2c6d54db66d6538c
Hamza Tahir <mhamzat0@gmail.com>
2023-12-26 14:10:57 -0800

    Finalized Download Page

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    let divMathOutput = document.querySelectorAll('#MathText');
-    for (let i = 0; i < divMathOutput.length; i++){
-        divMathOutput[i].innerHTML = katex.renderToString(divMathOutput[i].innerHTML);
+function isMobile() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+    if (!(isMobile())) {
+        var elements = document.getElementsByClassName("title");
+        for (var i = 0; i < elements.length;i++){
+            elements.item(i).style.fontSize = "1.5rem";
+        }
+    }
+    else{
+        document.getElementsByClassName("title_container").item(0).style.height = "40vh";
+        let buttons = document.getElementsByTagName("button");
+        for (var i = 0; i < buttons.length; i++){
+            buttons.item(i).style.fontSize = "1rem";
+        }
+        document.getElementsByClassName("button_text").item(0).style.fontSize = "3.5vw";
     }
 });
